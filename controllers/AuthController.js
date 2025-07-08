f582b51bb03dcacda3564b75b8fa04aa4ecca0d5
Mahdi <Mahdikgz7@gmail.com>
2025-07-08 21:33:57 +0330

    chore: hash the passwords

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 const UserModel = require("../models/UserModel");
 const registerValidator = require("./../validators/RegisterValidator");
+const bcrypt = require("bcryptjs");
 
 exports.Register = async (req, res) => {
   const validationResult = registerValidator(req.body);
@@ -8,12 +9,14 @@ exports.Register = async (req, res) => {
     return res.status(422).json({ message: "Check All the fileds!" });
   }
   let { name, username, email, password } = req.body;
+  // has password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const insertNewUser = await UserModel.create({
     name,
     username,
     email,
-    password,
+    password: hashedPassword,
     role: "USER",
   });
 
