a8812290b9bd9775a209c7facbff608cd4533165
Mahdi <Mahdikgz7@gmail.com>
2025-07-10 13:03:44 +0330

    refactor: userExistence for login

--- middlewares/userExistence.js
+++ middlewares/userExistence.js
@@ -1,13 +1,14 @@
 const UserModel = require("../models/UserModel");
 
 const userExists = async (req, res, next) => {
-  const { email } = req.body;
-  const existingUser = await UserModel.findOne({ email });
+  const { username } = req.body;
+  const existingUser = await UserModel.findOne({ username });
 
-  if (existingUser && req.url === "/register/") {
+  if (existingUser && req.url.includes("/register")) {
     return res.status(409).json({ message: "User already Exists" });
+  } else if (existingUser === null && req.url.includes("/login")) {
+    return res.status(404).json({ message: "User Not found!" });
   }
-
   return next();
 };
 
